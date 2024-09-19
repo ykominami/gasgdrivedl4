13693f30e2095a1c661d0949c1a87271085bad52
ykominami <ykominami@gmail.com>
2024-09-20 02:44:08 +0900

    daily_logx ,  MyBot-X

---
+++ src/@types/searchItem.d.ts
@@ -0,0 +1,10 @@
+import { Itemx } from './itemx';
+import { Itemvalue } from './itemvalue';
+export declare class SearchItem {
+    searches: Itemx[];
+    value: Itemvalue;
+    constructor(options: {
+        searches: Itemx[];
+        value: Itemvalue;
+    });
+}
